bea20e12b56c63229e04a01f89788fc6e9078936
ohhex123 <1728890680@qq.com>
2026-01-22 18:28:48 +0800

    complish

--- exercises/22_class_template/main.cpp
+++ exercises/22_class_template/main.cpp
@@ -10,6 +10,10 @@ struct Tensor4D {
     Tensor4D(unsigned int const shape_[4], T const *data_) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for(int i = 0;i < 4;i++){
+            shape[i] = shape_[i];
+            size *= shape[i];
+        }
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
     }
@@ -28,6 +32,25 @@ struct Tensor4D {
     // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
     Tensor4D &operator+=(Tensor4D const &others) {
         // TODO: 实现单向广播的加法
+        int dst_idx = 0;
+        for(int n = 0;n < shape[0];n++){
+            int n_other = (others.shape[0] == 1) ? 0 : n;
+            for(int c = 0;c < shape[1];c++){
+                int c_other = (others.shape[1] == 1) ? 0 : c;
+                for(int h = 0;h < shape[2];h++){
+                    int h_other = (others.shape[2] == 1) ? 0 : h;
+                    for(int w = 0;w < shape[3];w++){
+                        int w_other = (others.shape[3] == 1) ? 0 : w;
+                        int src_idx = w_other + 
+                        h_other * (others.shape[3]) + 
+                        c_other * (others.shape[2]) * (others.shape[3]) + 
+                        n_other * (others.shape[1]) * (others.shape[2]) * (others.shape[3]);
+                        data[dst_idx] += others.data[src_idx];
+                        dst_idx++;
+                    }
+                }
+            }
+        }
         return *this;
     }
 };
