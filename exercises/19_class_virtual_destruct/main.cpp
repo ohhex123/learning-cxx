b38f18a0e159f80912e75138f8abdab9c9dfb77d
ohhex123 <1728890680@qq.com>
2026-01-22 22:46:30 +0800

    virtual ~A

--- exercises/19_class_virtual_destruct/main.cpp
+++ exercises/19_class_virtual_destruct/main.cpp
@@ -10,7 +10,7 @@ struct A {
     A() {
         ++num_a;
     }
-    ~A() {
+    virtual ~A() {
         --num_a;
     }
 
@@ -38,7 +38,7 @@ int B::num_b = 0;
 int main(int argc, char **argv) {
     auto a = new A;
     auto b = new B;
-    ASSERT(A::num_a == 1, "Fill in the correct value for A::num_a");
+    ASSERT(A::num_a == 2, "Fill in the correct value for A::num_a");
     ASSERT(B::num_b == 1, "Fill in the correct value for B::num_b");
     ASSERT(a->name() == 'A', "Fill in the correct value for a->name()");
     ASSERT(b->name() == 'B', "Fill in the correct value for b->name()");
@@ -49,7 +49,7 @@ int main(int argc, char **argv) {
     ASSERT(B::num_b == 0, "Every B was destroyed");
 
     A *ab = new B;// 派生类指针可以随意转换为基类指针
-    ASSERT(A::num_a == 2, "Fill in the correct value for A::num_a");
+    ASSERT(A::num_a == 1, "Fill in the correct value for A::num_a");
     ASSERT(B::num_b == 1, "Fill in the correct value for B::num_b");
     ASSERT(ab->name() == 'B', "Fill in the correct value for ab->name()");
 
